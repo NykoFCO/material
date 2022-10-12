38aaacc9c369259defc65bf7cc724b062347b9e1
Adolfo Sanchez <nykoprofe@gmail.com>
2022-10-11 23:24:40 -0300

    desde casa minuto32

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { EncabezadoComponent } from './componentes/encabezado/encabezado.compone
 import { AboutComponent } from './componentes/about/about.component';
 import { MiportfolioComponent } from './componentes/miportfolio/miportfolio.component';
 import { ContactameComponent } from './componentes/contactame/contactame.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ContactameComponent } from './componentes/contactame/contactame.compone
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
